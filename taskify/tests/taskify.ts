ee8641473c00cc84ece63482ecc89b41fb203615
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-17 13:47:33 +0530

    test for add working fine

--- taskify/tests/taskify.ts
+++ taskify/tests/taskify.ts
@@ -3,7 +3,7 @@ import { Program } from "@coral-xyz/anchor";
 import { Taskify } from "../target/types/taskify";
 import { assert } from "chai";
 
-describe("taskify", async () => {
+describe("taskify", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -11,8 +11,12 @@ describe("taskify", async () => {
   const program = anchor.workspace.taskify as Program<Taskify>;
   const mainKeyPair = anchor.web3.Keypair.generate();
 
-  //airdrop the solana 
-  await provider.connection.requestAirdrop(mainKeyPair.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL);
+  before(
+    //function to put on the airdrop 
+    async () => {
+      const sign = await provider.connection.requestAirdrop(mainKeyPair.publicKey, 20 * anchor.web3.LAMPORTS_PER_SOL);
+      await provider.connection.confirmTransaction(sign, "confirmed")
+    })
 
   //make the seed Prefix 
   const seedprefix = Buffer.from("hello_word");
@@ -28,18 +32,37 @@ describe("taskify", async () => {
   it("Is initialized!", async () => {
     const data = "marco"
     const [pda] = derivePda();
+    console.log("pda is : " + pda);
     // Add your test here.
-    await program.methods.add(data).accounts({
-      payer: mainKeyPair.publicKey
+    const tx = await program.methods.add(data).accounts({
+      payer: mainKeyPair.publicKey,
     }).signers([mainKeyPair]).rpc();
 
-    const account = await program.account.customAccount.fetch(pda);
-    assert.equal(account.data, data);
+    const value = await provider.connection.confirmTransaction(tx);
+    console.log("value is : ");
+    console.log(value);
+    const account = await program.account.customAccount.all();
+    console.log("account: ");
+    console.log(account);
   });
 
-  //for update function 
+
+  //for updating 
   // it("is updating", async () => {
-  //   const data = "ping"
   //
+  //   const accounts = await provider.connection.getProgramAccounts(mainKeyPair.publicKey)
+  //   console.log(accounts);
+  //
+  //   const pda = "9GxLZnyNjK5hkUuGzPQFbYTQzpXfq2pZGBLASktFu6L4"
+  //   // await the program 
+  //   await program.methods.update("figarland").accounts({
+  //     signer: pda
+  //   }).signers([mainKeyPair]).rpc();
+  //   const account = await program.account.customAccount.fetch(pda)
+  //
+  //   //assert 
+  //   assert.equal(account.data, "figarland");
   // })
+
+
 });
