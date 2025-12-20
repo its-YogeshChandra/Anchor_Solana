1a641627e74b1168f21dd668c40d95d2f957513a
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-20 17:34:31 +0530

    added createtokenmint & added createtokenaddress creator code in contract of token mint

--- tokenmint/programs/tokenmint/src/lib.rs
+++ tokenmint/programs/tokenmint/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 declare_id!("BaJwzP9zYpBtfFsKvK1YKS8qwAgFc1haABobpgWmUu2R");
 
@@ -10,7 +13,16 @@ pub mod tokenmint {
 
     //create mint address
     pub fn createmint(ctx: Context<CreateMint>, decimals: u8) -> Result<()> {
-        msg!("mint created");
+        msg!("mint created : {:?}", ctx.accounts.mint.key());
+        Ok(())
+    }
+
+    //create mint account
+    pub fn create_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
+        msg!(
+            "created token account : {:?}",
+            ctx.accounts.token_account.key()
+        );
         Ok(())
     }
 }
@@ -28,6 +40,21 @@ pub struct CreateMint<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// pub struct DecimalValue {
-//     data: u8,
-// }
+//create token account
+//done through making a pda derived from and creeated by associated token program.
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    #[account(mut)]
+    //check that process is done for the same address who has signed the transaction
+    pub signer: Signer<'info>,
+    #[account(init_if_needed, payer = signer, associated_token::mint = mint, associated_token::authority = signer, associated_token::token_program = token_program)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+//extras
+//mint address :
+//from keypair or from program derived address
