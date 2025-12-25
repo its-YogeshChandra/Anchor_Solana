ff06aac208dfc17c7d97ab35d3dd6753edee5c87
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-25 17:49:38 +0530

    create mint function working fine

--- vault-program/programs/vault-program/src/lib.rs
+++ vault-program/programs/vault-program/src/lib.rs
@@ -1,4 +1,7 @@
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::{pubkey::ParsePubkeyError, *};
+use anchor_spl::token_interface::{
+    self, Mint, MintTo, TokenAccount, TokenInterface, TransferChecked,
+};
 
 declare_id!("4h2c3Fdre59qUXW2keiDhTZy5wbtKgxBT8wZDfsrt3Jd");
 
@@ -6,11 +9,43 @@ declare_id!("4h2c3Fdre59qUXW2keiDhTZy5wbtKgxBT8wZDfsrt3Jd");
 pub mod vault_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<CreateMint>, decimals: u8) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    //function to create the token account using the pda through user
+    pub fn create_ata(ctx: Context<CreateTokenAccount>) -> Result<()> {
+        msg!("token acount : {:?}", ctx.accounts.token_account);
         Ok(())
     }
 }
 
+//function to create mint
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(decimals: u8 )]
+pub struct CreateMint<'info> {
+    // who gonna pay for this
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    //the macro to initialize the mint process
+    #[account( init , payer = payer, mint::decimals = decimals , mint::authority = payer.key(), mint::freeze_authority = payer.key())]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+//function to create ata that is owned by a pda
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    //payer of the transaction
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    // the macro to create the ata
+    #[account(init , payer = payer, token::mint = mint, token::authority = token_account ,token::token_program = token_program, seeds = [b"vault", payer.key().as_ref()], bump)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
--- vault-program/tests/vault-program.ts
+++ vault-program/tests/vault-program.ts
@@ -1,16 +1,42 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VaultProgram } from "../target/types/vault_program";
-
+import { LAMPORTS_PER_SOL, } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, getMint, getAccount } from "@solana/spl-token";
 describe("vault-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
+  const mintKeyPair = anchor.web3.Keypair.generate();
+  const userWallet = anchor.web3.Keypair.generate();
   const program = anchor.workspace.vaultProgram as Program<VaultProgram>;
+
+  before(async () => {
+    const airdrpSig = await provider.connection.requestAirdrop(userWallet.publicKey, 30 * LAMPORTS_PER_SOL);
+    await provider.connection.confirmTransaction(airdrpSig)
+  })
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize(9).accounts({
+      payer: userWallet.publicKey,
+      mint: mintKeyPair.publicKey,
+      tokenProgram: TOKEN_PROGRAM_ID
+    }).signers([mintKeyPair, userWallet]).rpc({ commitment: "confirmed" });
+
+    //find the mint account from the user userWallet 
+    const mintAccount = await getMint(provider.connection,
+      mintKeyPair.publicKey,
+      "confirmed",
+      TOKEN_PROGRAM_ID)
+
+    console.log("mint Account : ")
+    console.log(mintAccount)
+
+    console.log("mint publickey: ")
+    console.log(mintKeyPair.publicKey)
   });
+
+
 });
