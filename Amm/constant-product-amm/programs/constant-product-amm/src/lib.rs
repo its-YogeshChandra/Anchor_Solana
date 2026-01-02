8d1e874411b86598e6b26d8b2066418a99e36ad9
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-03 00:41:53 +0530

    added single function way to make lp

--- Amm/constant-product-amm/programs/constant-product-amm/src/lib.rs
+++ Amm/constant-product-amm/programs/constant-product-amm/src/lib.rs
@@ -69,48 +69,79 @@ pub struct CreateToken<'info> {
 #[account]
 #[derive(InitSpace)]
 pub struct LpPoolAccountShape {
-    usdc_vault_address: Pubkey,
-    sol_vault_address: Pubkey,
-    lp_token_mint: Pubkey,
+    pub usdc_mint: Pubkey,
+    pub sol_mint: Pubkey,
+    pub usdc_vault_address: Pubkey,
+    pub sol_vault_address: Pubkey,
+    pub lp_token_mint: Pubkey,
+    pub bump: u8,
 }
 
 //deriving function
+//two ways: (i) seperate function, (ii) single function
+//(i)seperate function
+// #[derive(Accounts)]
+// pub struct CreateLPPoolState<'info> {
+//     //init a data account on a pda
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+//     #[account(init , payer = signer, space = 8+LpPoolAccountShape::INIT_SPACE, seeds = [b"lpstate", signer.key().as_ref()], bump)]
+//     pub pool_account: Account<'info, LpPoolAccountShape>,
+//     pub system_program: Program<'info, System>,
+// }
+//
+// //usdc vault
+// #[derive(Accounts)]
+// pub struct UsdcVault<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+//     pub mint: InterfaceAccount<'info, Mint>,
+//     //owner of this account
+//     pub pool_state: Account<'info, LpPoolAccountShape>,
+//
+//     //the macro to create  the ata
+//     #[account(init, payer = signer, token::mint = mint, token::authority = pool_state, token::token_program = token_program, seeds = [b"usdc_vault", mint.key().as_ref()], bump)]
+//     pub token_account: InterfaceAccount<'info, TokenAccount>,
+//     pub token_program: Interface<'info, TokenInterface>,
+//     pub system_program: Program<'info, System>,
+// }
+//
+// //sol vault
+// #[derive(Accounts)]
+// pub struct SolVault<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+//     pub mint: InterfaceAccount<'info, Mint>,
+//
+//     //owner of the this account
+//     pub pool_state: Account<'info, LpPoolAccountShape>,
+//     //create ata
+//     #[account(init, payer= signer, token::mint = mint, token::authority = pool_state, token::token_program = token_program, seeds = [b"solana_vault", mint.key().as_ref()], bump)]
+//     pub token_account: InterfaceAccount<'info, TokenAccount>,
+//     pub token_program: Interface<'info, TokenInterface>,
+//     pub system_program: Program<'info, System>,
+// }
+
+//single function
 #[derive(Accounts)]
 pub struct CreateLPPoolState<'info> {
-    //init a data account on a pda
+    //payer of the account
     #[account(mut)]
     pub signer: Signer<'info>,
-    #[account(init , payer = signer, space = 8+LpPoolAccountShape::INIT_SPACE, seeds = [b"lpstate", signer.key.as_ref()], bump)]
-    pub pda_account: Account<'info, LpPoolAccountShape>,
-    pub system_program: Program<'info, System>,
-}
-
-//make impl block and add function to CreateLPPoolState
-
-//usdc vault
-#[derive(Accounts)]
-pub struct UsdcVault<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    pub mint: InterfaceAccount<'info, Mint>,
-
-    //the macro to create  the ata
-    #[account(init, payer = signer, token::mint = mint, token::authority = token_account, token::token_program = token_program, seeds = [b"usdc_vault", signer.key().as_ref()], bump)]
-    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub usdc_mint: InterfaceAccount<'info, Mint>,
+    pub sol_mint: InterfaceAccount<'info, Mint>,
     pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>,
-}
 
-//sol vault
-#[derive(Accounts)]
-pub struct SolVault<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    pub mint: InterfaceAccount<'info, Mint>,
-
-    //create ata
-    #[account(init, payer= signer, token::mint = mint, token::authority = token_account, token::token_program = token_program, seeds = [b"solana_vault", signer.key().as_ref()], bump)]
-    pub token_account: InterfaceAccount<'info, TokenAccount>,
-    pub token_program: Interface<'info, TokenInterface>,
+    //create the account first
+    #[account(init, payer = signer, space = 8 + LpPoolAccountShape::INIT_SPACE, seeds  = [b"lpstate", signer.key().as_ref()], bump)]
+    pub pool_account: Account<'info, LpPoolAccountShape>,
     pub system_program: Program<'info, System>,
+
+    //usdc_vault
+    #[account(init, payer= signer, token::mint = usdc_mint, token::authority = pool_account, token::token_program = token_program, seeds = [b"usdc_vault", usdc_mint.key().as_ref()], bump)]
+    pub usdc_account: InterfaceAccount<'info, TokenAccount>,
+
+    //solana vault
+    #[account(init, payer = signer,token::mint = sol_mint, token::authority = pool_account, token::token_program = token_program, seeds = [b"solana_vault", sol_mint.key().as_ref()], bump)]
+    pub solana_account: InterfaceAccount<'info, TokenAccount>,
 }
